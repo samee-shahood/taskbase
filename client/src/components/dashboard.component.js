014601597d8c4aaea1ebf7bf9dd5194c49135dca
Samee Shahood <sameeshahood11@gmail.com>
2021-03-06 16:07:26 -0500

    Format grid

--- client/src/components/dashboard.component.js
+++ client/src/components/dashboard.component.js
@@ -1,61 +1,94 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import "./dashboard.css";
+
+import { Card, ListGroup, Col, Row } from 'react-bootstrap';
+
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 import Axios from 'axios';
 
-export default class Dashboard extends Component{
+const Dashboard =() =>{
 
-    constructor(props){
-        super(props);
+	const [startDate, setStartDate] = useState(new Date());
 
-        this.state = {
-            
-        }
-    }
+	let tasks = [{
+		date: "03/06/2021",
+		title: "Integrate API",
+		description: "i need to unhard code this"	
+	},
 
+	{
+		date: "03/07/2021",
+		title: "Submit",
+		description: "need to submit this!"	
+	}];
 
-    componentDidMount() {
-        console.log('reloaded /search');
-    } 
-
-	async componentWillReceiveProps(props){
-		console.log("nothing");
-    } 
-
-    render(){
        
-        return(
-                <div>
-					
-					
-                    <div class="wrapper">
-						<div class="one">
-							One
-						</div>
-
-						<div class="two">
-							Two
-						</div>
-
-						<div class="three">
-							Three
-						</div>
-
-						<div class="four">
-							Four
-						</div>
-
-						<div class="five">
-							Five
-						</div>
-
-						<div class="six">
-							Six
-						</div>
+	return(
+			<div>
+				<div class="wrapper">
+					<div class="one">
+						One
 					</div>
 
-                </div>
-        )
-    }
+					<div class="two">
+						Two
+					</div>
 
-}
+					<div class="three">
+					<Card.Header as="h5">
+						Your Task List
+					
+					<DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+					</Card.Header>
+					<ListGroup variant="flush">
+
+						{tasks.map((item =>
+							<ListGroup.Item>
+								
+								{/* <Card style={{ width: '18rem' }}> */}
+									<Card.Body>
+										<Row>
+											<Col md={5}>
+												<Card.Title>{item.title}</Card.Title>
+											</Col>
+											<Col xs={3}></Col>
+											<Col>
+												<Card.Title>{item.date}</Card.Title>
+											</Col>
+										</Row>
+										
+										{/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
+										<Card.Text>
+										{item.description}
+										</Card.Text>
+									</Card.Body>
+								{/* </Card> */}
+
+							</ListGroup.Item>
+						))}
+					</ListGroup>
+
+					</div>
+
+					<div class="four">
+						Four
+					</div>
+
+					<div class="five">
+						Five
+					</div>
+
+					<div class="six">
+						Six
+					</div>
+				</div>
+
+			</div>
+	)
+
+};
+
+export default Dashboard;
