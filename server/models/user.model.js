91cd8e50e5e3f1356e8d63aa73b45eb4934a0091
Samee Shahood <sameeshahood11@gmail.com>
2021-03-06 12:11:27 -0500

    Add user duplication check

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
 	username: {
 		type: String,
-		default: ""
+		default: "",
+		unique: true,
 	},
 	email: {
 		type: String,
 		default: ""
+		unique: true
 	},
 	password: {
 		type: String,
