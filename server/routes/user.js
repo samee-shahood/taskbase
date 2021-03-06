3eb481534af707bc40f2206fdb32a591a3cc378c
Samee Shahood <sameeshahood11@gmail.com>
2021-03-06 12:32:31 -0500

    Update website title

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 let User = require('../models/user.model');
 const app = express.Router();
-const passport = require("passport")
+const passport = require("passport");
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken")
 const saltRounds = 10;
 
 app.post("/users/register", async (req, res) => {
@@ -23,9 +24,41 @@ app.post("/users/register", async (req, res) => {
 				}
 			res.send(newUser);
 		});
-	
 	});
 
 });
+
+app.post("/login", (req, res, next) => {
+	passport.authenticate("local", (err, user) => {
+		if(err){
+			return next(err)
+		}
+		if(!user){
+			return res.status(401).send({ success: false, message: 'Invalid Login' });
+		}
+		req.login(user, () => {
+			const body = {_id: user.id, email: user.email }
+	
+			const token = jwt.sign({user: body}, "jwt_secret")
+			return res.json({token})
+		})
+	})(req, res, next)
+})
+  
+app.get("/secret", passport.authenticate("jwt", { session: false }), (req, res) => {
+if(!req.user){
+	res.json({
+	username: "nobody"
+	})
+} else {
+	res.json(req.user)
+}
+})
+
+app.get('/logout', function(req, res){
+	req.logout();
+	return res.json({success: "true"});
+});
+  
 
 module.exports = app;
