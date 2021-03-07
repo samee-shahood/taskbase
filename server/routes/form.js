47f1a066f3f241b1dcc8f4b45de773cb255de4e7
Samee Shahood <sameeshahood11@gmail.com>
2021-03-06 23:28:01 -0500

    Working on typeform

--- server/routes/form.js
+++ server/routes/form.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user.model');
 const fetch = require("node-fetch");
 
 router.post("/form/:id", async (req, res) => {
