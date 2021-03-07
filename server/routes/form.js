2171a1a5f6ca7be66a3e3fc4c0f6e854ccddb6e5
Samee Shahood <sameeshahood11@gmail.com>
2021-03-07 00:11:48 -0500

    Add typeform

--- server/routes/form.js
+++ server/routes/form.js
@@ -2,48 +2,56 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.model');
 const fetch = require("node-fetch");
+const passport = require("passport");
 
-router.post("/form/:id", async (req, res) => {
-    // unique form submission ID
-    const responseID = req.params.id
-    console.log(responseID);
 
-    // let user = await User.findOne({discordID: req.user.id});
-    // user.responseID = responseID;
+router.get(
+	"/form/:id", 
+	passport.authenticate("jwt", { session: false }),
+	async (req, res) => {
+		// unique form submission ID
+		const responseID = req.params.id
 
-    const token = "CPFGBMMq987qT4f9SNDqBLfZSFjd17dTsQwEYwKqXqiJ";
+		const user = await User.findById(req.user.id)
+		.catch((err)=>{
+			return res.status(400).send(err);
+		});
 
-    setTimeout(() => {
-      fetch('https://api.typeform.com/forms/PMReobBB/responses?' + new URLSearchParams({
-      included_response_ids: responseID,
-      }), {
-            method: 'GET',
-            headers: {
-            'authorization': `bearer ${token}`
-    }}).then(res => res.json())
-    .then(resJson => {
-      const responses = resJson.items[0].answers;
-	//   let task = {
-	// 	  elem
-	//   }
-    //   responses.forEach((elem) => {
-    //     if (elem.type == 'text') {
-    //       user.name = elem.text;
-    //     }
-    //     if (elem.type ==  'url') {
-    //       user.github = elem.url;
-    //     }
-    //     if (elem.type == 'choices') {
-    //       user.genres = elem.choices.labels;
-    //     }
-    //   })
-	console.log(responses);
-    //   user.save();
-    });
-    }, 1000)
+		if(user == undefined){
+			return res.status(404).send("User not found");
+		}
 
-    
-    res.send(200);
+		const token = "CPFGBMMq987qT4f9SNDqBLfZSFjd17dTsQwEYwKqXqiJ";
+
+		setTimeout(() => {
+		fetch('https://api.typeform.com/forms/PMReobBB/responses?' + new URLSearchParams({
+		included_response_ids: responseID,
+		}), {
+				method: 'GET',
+				headers: {
+				'authorization': `bearer ${token}`
+		}}).then(res => res.json())
+		.then(resJson => {
+		const responses = resJson.items[0].answers;
+		let task = {};
+		responses.forEach((elem) => {
+			if (elem.field.type == 'short_text') {
+				task.title = elem.text;
+			}
+			if (elem.field.type == 'long_text') {
+				task.description = elem.text;
+			}
+			if (elem.type == 'date') {
+				task.date = new Date(elem.date.replace(/-/g, '\/').replace(/T.+/, ''));
+			}
+		})
+			user.tasks.push(task);
+		  	user.save();
+		});
+		}, 1000)
+
+		
+		res.send(200);
 });
 
 module.exports = router;
