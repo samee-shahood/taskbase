50bf3f3dc1c421c877b08f6df675adc95f7b3db2
Anees Aissaoui <31697581+aaiss1@users.noreply.github.com>
2021-03-06 20:50:15 -0500

    Add changes to weather component, wip

--- client/src/components/weather.component.js
+++ client/src/components/weather.component.js
@@ -1,6 +1,7 @@
 ﻿import React, { Component } from 'react';
 import axios from 'axios';
-
+import { Card, ListGroup, Col, Row } from 'react-bootstrap';
+import "./dashboard.css";
 
 export default class Weather extends Component {
     constructor(props) {
@@ -35,9 +36,42 @@ export default class Weather extends Component {
 
 	render(){
 		return(
-			<div>
-				<h>{this.state.location}</h>
-				<img src={this.state.icon} />
+			<div style={{ 
+				backgroundImage: `url("https://fasken.azureedge.net/-/media/29b8b6a371344d40a392350b395691d6.ashx?mw=2560&modified=20200624134121&hash=69328985F09305B8D5FFD44766B77D53")` 
+			  }}>
+				  
+				<ListGroup variant="flush">
+								<ListGroup.Item>
+									
+									{/* <Card style={{ width: '18rem' }}> */}
+									<Card.Img id="city" src="https://fasken.azureedge.net/-/media/29b8b6a371344d40a392350b395691d6.ashx?mw=2560&modified=20200624134121&hash=69328985F09305B8D5FFD44766B77D53" alt="Card image" />
+									<Card.ImgOverlay>
+										<Card.Body>
+											<Row>
+												<Col md={7}>													
+													<Card.Title>{this.state.location}</Card.Title>
+													<Row>
+														<Col md={3}>
+															<img src={this.state.icon} />
+														</Col>
+
+														<Col md={7}>
+															<Card.Text id="WeatherDesc">
+																{this.state.forecast}
+															</Card.Text>
+														</Col>
+													</Row>
+												</Col>
+												<Col xs={2}></Col>
+												<Col>
+													<Card.Title id="temp">{this.state.temperature} °C</Card.Title>
+												</Col>
+											</Row>
+										</Card.Body>
+									</Card.ImgOverlay>
+								</ListGroup.Item>
+		
+						</ListGroup>
 			</div>
 		);
 	};
